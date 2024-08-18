805edeee42a0324bfffbe1c9ef87b16662976128
Lukasz Siwinski <lsiwinski@gmail.com>
2024-08-18 23:10:04 +0200

    Enable goals: package, verify, install and site.
    Intentionally introduced some style/code bugs to check if cpd, checkstyle, pmd and tag list are working properly when generating project site.
    
    Step deploy is still not possible - additional GitHub and maven configuration is needed, but this can be postponed as for now :)
    
     The default goal doesn't work as well, this can be postponed as for now as well.

--- src/main/java/io/github/hopbit/liveoddsservices/HelloSportradar.java
+++ src/main/java/io/github/hopbit/liveoddsservices/HelloSportradar.java
@@ -1,7 +1,15 @@
 package io.github.hopbit.liveoddsservices;
 
-class HelloSportradar {
+/**
+ * HelloSportradar class.
+ * @deprecated
+ */
+public class HelloSportradar {
     public static void main(String[] args) {
         System.out.println("Hello, Sportradar!");
+        String spotBugsTest = null;
+        spotBugsTest.length();
     }
+    // TODO: BLA BLA BLA
+    // FIXME: LOREM IPSUM DOLOR
 }
